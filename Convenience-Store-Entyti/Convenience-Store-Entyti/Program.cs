9f82b4d5d4d3f24ce3ab289046f2ae1f8e4a463f
Cao Nhân <nguyencaonhanthanquyle@gmail.com>
2023-04-19 22:44:10 +0700

    type,stock,supplier,manuafacture

--- Convenience-Store-Entyti/Convenience-Store-Entyti/Program.cs
+++ Convenience-Store-Entyti/Convenience-Store-Entyti/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Convenience_Store_Entyti.DanhMuc;
 namespace Convenience_Store_Entyti
 {
     internal static class Program
@@ -16,7 +16,11 @@ namespace Convenience_Store_Entyti
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new FrmMain());
+            //Application.Run(new FrmManuafacture());
+            //Application.Run(new FrmStock());
+            //Application.Run(new FrmSupplier());
+            Application.Run(new FrmType());
         }
     }
 }
